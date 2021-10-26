b34949ba170a2f37665a116a852d27d6ee76e965
Gabriele Batagiero <79870386+GabrieleBatagiero@users.noreply.github.com>
2021-10-26 09:45:02 -0300

    Add files via upload

--- calculdora.js
+++ calculdora.js
@@ -3,8 +3,10 @@ var num2 = parseInt(prompt('Digite o num2'));
 
 var soma = num1 + num2;
 
+var subtracao = num1 - num2;
 var multiplicacao = num1 * num2;
-var divisao = num1  num2;
+var divisao = num1 / num2;
+
 
 document.getElementById('num1').innerHTML = num1;
 document.getElementById('num2').innerHTML = num2;
