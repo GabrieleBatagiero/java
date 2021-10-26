bf62e52d679dd236681267c522539b677cab5655
Gabriele Batagiero <79870386+GabrieleBatagiero@users.noreply.github.com>
2021-10-26 09:35:30 -0300

    Add files via upload

---
+++ exemplo1.js
@@ -0,0 +1,9 @@
+texto = "vamos fazer uma calculadora";
+num1 = 25;
+num2 = 26;
+soma = num1+num2
+alert(soma);
+console.log (num1);
+//document.write(texto);
+
+document.getElementById("teste").innerHTML = texto;
